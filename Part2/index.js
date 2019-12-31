b2fc45a3d463cb02e153ac1d7b000fa76d1b8950
Benjamin Dehlin <ben.dehlin@gmail.com>
2019-12-30 19:13:13 -0700

    started part 2

---
+++ Part2/index.js
@@ -0,0 +1,11 @@
+console.log('hello world')
+
+const idInput = document.getElementById('idInput')
+const colorInput = document.getElementById('colorInput')
+
+
+function setCard(){
+  const card = document.getElementById(idInput.value)
+  card.style.color = colorInput.value
+  console.log(card)
+}
